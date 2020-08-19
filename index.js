4c401ac851e27225756ca9d515ba48769a0ebdf9
edcaron <eduardocaron10@gmail.com>
2020-08-18 23:01:53 -0300

    remove ;

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-import express, { json } from 'express';
-import cors from 'cors';
-import { connect, connection as _connection } from 'mongoose';
-import todosRouter from './routes/todos';
-require('dotenv').config();
+import express, { json } from 'express'
+import cors from 'cors'
+import { connect, connection as _connection } from 'mongoose'
+import todosRouter from './routes/todos'
+require('dotenv').config()
 
-const app = express();
-const port = process.env.PORT || 5000;
-app.use(cors());
-app.use(json());
+const app = express()
+const port = process.env.PORT || 5000
+app.use(cors())
+app.use(json())
 
-const uri = process.env.ATLAS_URI;
-connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const uri = process.env.ATLAS_URI
+connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
 
-const connection = _connection;
+const connection = _connection
 connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
+  console.log("MongoDB database connection established successfully")
 })
 
 app.use('/todos', todosRouter)
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-});
+    console.log(`Server is running on port: ${port}`)
+})
